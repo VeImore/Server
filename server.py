891f09d7de88afbbc6ba451df886aa54f20315f9
toyol <toyol@Toyolvski>
2023-09-12 13:02:37 -0700

    final commit

--- server.py
+++ server.py
@@ -13,18 +13,33 @@ AWESOMENESS = [
     'oh-so-not-meh', 'brilliant', 'ducky', 'coolio', 'incredible',
     'wonderful', 'smashing', 'lovely']
 
+DISSLIST = [
+    'jiveturkey', 'smelly', 'yo mama', 'deez', 'whack', 'butthead', 'stupid'
+    'lame'
+]
+
 
 @app.route('/')
 def start_here():
     """Home page."""
 
-    return "<!doctype html><html>Hi! This is the home page.</html>"
+    return """
+      <!doctype html>
+      <html>
+        <head>
+          <title>Free $100 click here</title>
+        </head>
+        <body>
+          <h1>Hi! This is the home page.</h1>
+          <a href = "/hello">Click here to visit the 'hello' page!</a>
+        </body>
+      </html>
+      """
 
-
+    
 @app.route('/hello')
 def say_hello():
     """Say hello and prompt for user's name."""
-
     return """
     <!doctype html>
     <html>
@@ -35,8 +50,28 @@ def say_hello():
         <h1>Hi There!</h1>
         <form action="/greet">
           What's your name? <input type="text" name="person">
+          </br>
+          What compliment would you like to choose?
+          </br>
+          <input type ="radio" name ="compliment" value = "awesome"> Awesome </br>
+          <input type ="radio" name ="compliment" value = "terrific"> Terrific </br>
+          <input type ="radio" name ="compliment" value = "fantastic"> Fantastic </br>
+          <input type ="radio" name ="compliment" value = "neato" > Neato </br>
+          <input type ="radio" name ="compliment" value = "fantabulous" > Fantabulous </br>
+          <input type ="radio" name ="compliment" value = "oh-so-not-meh" > Oh-So-Not-Meh</br>
+          <input type ="radio" name ="compliment" value = "wonderful"> Wonderful</br>
+          <input type ="radio" name ="compliment" value = "wowza"> Wowza</br>
+          <input type ="radio" name ="compliment" value = "smashing"> Smashing</br>
+          <input type ="radio" name ="compliment" value = "lovely"> Lovely</br>
+          </br>
           <input type="submit" value="Submit">
         </form>
+        <form action="/diss">
+          What's your name to get DISSED? <input type="text" name="person">
+          </br>
+          <input type="submit" value="Submit">
+        </form>
+        
       </body>
     </html>
     """
@@ -47,8 +82,9 @@ def greet_person():
     """Get user by name."""
 
     player = request.args.get("person")
+    compliment = request.args.get("compliment")
 
-    compliment = choice(AWESOMENESS)
+    ## compliment = choice(AWESOMENESS)
 
     return f"""
     <!doctype html>
@@ -62,6 +98,24 @@ def greet_person():
     </html>
     """
 
+@app.route('/diss')
+def say_diss():
+    """Dissing you like it is 1995 """
+
+    player = request.args.get("person")
+    diss = choice(DISSLIST)
+
+    return f"""
+    <!doctype html>
+    <html>
+      <head>
+        <title>A Big Fat Diss!</title>
+      </head>
+      <body>
+        Hi, {player}! I think you're {diss}!
+      </body>
+    </html>
+    """
 
 if __name__ == '__main__':
     # debug=True gives us error messages in the browser and also "reloads"
